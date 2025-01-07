a048aeff98d5556204ab683aad663be74388f46f
Rajas Bhosale <rajasvbhosale@gmail.com>
2025-01-07 19:29:44 +0530

    added the text effects

--- src/Components/Video_Carousel.jsx
+++ src/Components/Video_Carousel.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Video_Carousel.css"; // Import your CSS for the sliding effect
-import video1 from "../assets/Video/video1_carousel.mp4"
 import video2 from "../assets/Video/valo1.mp4"
 import video3 from "../assets/Video/valo2.mp4"
 import video4 from "../assets/Video/valo3.mp4"
