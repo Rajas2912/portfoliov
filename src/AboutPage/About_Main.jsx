621e6a6f50c773b1f4a6104cdbb92b1963dcead8
Rajas Bhosale <rajasvbhosale@gmail.com>
2025-01-22 23:02:23 +0530

    Edited the About Page

--- src/AboutPage/About_Main.jsx
+++ src/AboutPage/About_Main.jsx
@@ -2,6 +2,7 @@ import Footer from "../Components/Footer";
 import { NewNavbar } from "../FramerMotion/NewNavbar";
 import { useEffect, useRef } from "react";
 import Lenis from "lenis";
+import about_img from "../assets/images/prof23.jpg"
 import About_section from "./About_section";
 function About_Main()
 {
@@ -21,15 +22,28 @@ function About_Main()
         <NewNavbar></NewNavbar>
         <div className="about_main" style={{height:"100vh"}}>
         <div className="container" style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
-          <div className="f12" style={{ margin: "60px 183px" }}>
+          <div className="f12" style={{ margin: "60px 183px" ,display:"flex"}}>
+            <div>
             <div
               style={{ color: "black", fontSize: "2rem", fontFamily: "Montserrat-Regular" }}
             >
               About.
             </div>
-            <div className="me_1"style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+            <div className="me_1"style={{display:"flex",alignItems:"center",justifyContent:"center",fontSize:"1.2rem",paddingRight:"80px"}}>
             I’m Rajas Bhosale, skilled in AI, Machine Learning, Python, Data Structures and Algorithms (DSA), and Full-Stack Development. I have a passion for exploring new technologies and applying my skills to solve real-world problems.
             </div>
+            </div>
+            <div>
+              <img
+                              src={about_img}
+                              
+                              alt="Bootstrap Themes"
+                              width="400"
+                              height="500"
+                              loading="lazy"
+                            />
+            </div>
+
           </div>
           
           
